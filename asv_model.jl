e9861c76f3ecadca778337d5df2fe2ae5f159df8
kmgovind <kavin@liquidrocketry.com>
2023-09-20 08:36:44 -0400

    Started two-state model

--- asv_model.jl
+++ asv_model.jl
@@ -9,8 +9,19 @@ export ASV
 struct Boat{T<:Real}
     x::T # position
     b::T # battery state of charge
+
+    max_speed = 2.315; # m/s
+    min_speed = 0; # m/s
+
+    b_max = 6.5e3; # 6.5 KWh battery
+    b_min = 0; # 0 KWh battery
+    k_h = 200; # Hotel Load
+    k_m = 83; # Motor multiplier, need to tune
     
-    function boat(x,b)
+    function move_boat(x,b,dt)
         
+    end
+
+    function use_charge(x,b,dt)
     end
 end
